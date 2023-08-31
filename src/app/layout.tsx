ef291411375cafbd4148b0b0fc7b537a4a1c06b5
byoon97 <byoon5397@gmail.com>
2023-08-31 18:57:22 -0400

    created apollo server and connected it on client side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ApolloWrapper } from "../../lib/apollo-wrapper";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -10,7 +11,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {" "}
+        <ApolloWrapper>{children}</ApolloWrapper>
+      </body>
     </html>
   );
 }
