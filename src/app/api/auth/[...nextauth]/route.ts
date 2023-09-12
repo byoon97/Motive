2ff65e1442f49930492c9f7e937fd74c6a657945
byoon97 <byoon5397@gmail.com>
2023-09-11 22:59:17 -0400

    dev

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -84,7 +84,7 @@ export const authOptions: NextAuthOptions = {
       return token
     }
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 }
 
 const handler = NextAuth(authOptions)
